b623d07dd118b696625221f258beec57a5e362d5
Aswin K R <aswinkr77@gmail.com>
2021-04-28 22:27:28 +0530

    nothing much

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      //'2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      //'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1024px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '768px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '640px'},
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
