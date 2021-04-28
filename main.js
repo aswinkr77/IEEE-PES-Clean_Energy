92be571a0a9ab8ce03cd81d23e40a34ac4ed83b1
Aswin K R <aswinkr77@gmail.com>
2021-04-28 22:36:45 +0530

    Updates

---
+++ main.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+document.getElementById("toggleButton").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "flex";
+});
+
+document.getElementById("close").addEventListener("click", () => {
+    document.getElementById("modal").style.display = "none";
+});
+
+let modalMenus = document.querySelectorAll(".modal-menu a");
+
+modalMenus.forEach(function (item) {
+    item.addEventListener("click", () => {
+      document.getElementById("modal").style.display = "none";
+    });
+});
